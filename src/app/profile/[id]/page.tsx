20b9395830244d907affd1fec731b6e17d68ad8e
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2025-02-05 22:47:04 +0530

    Update

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 
-export default function UserProfile( {params}: any ) {
+export default function UserProfile({ params }: any) {
   return (
-    <div className='flex flex-col items-center justify-center gap-2 min-h-screen py-2'>
-     <h1>Profile</h1>
-     <hr />
-     <p className='text-4xl'>Profile page 
-        <span className=' p-1 font-mono btnBgColor rounded-md mx-2'>{params.id}</span>
-     </p>
+    <div className="flex flex-col items-center justify-center gap-2 min-h-screen py-2">
+      <h1>Profile</h1>
+      <hr />
+      <p className="text-4xl">
+        Profile page
+        <span className=" p-1 font-mono btnBgColor rounded-md mx-2">
+          {params.id}
+        </span>
+      </p>
     </div>
-  )
+  );
 }
--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,21 +1,19 @@
 import mongoose from "mongoose";
 
 export async function connect() {
+  try {
+    mongoose.connect(process.env.MONGO_URI!);
+    const connection = mongoose.connection;
 
-    try {
-        mongoose.connect(process.env.MONGO_URI!)
-        const connection = mongoose.connection;
-
-        connection.on('connected', () => {
-            console.log('MongoDB connected successfully'); 
-        })
-        connection.on('error', (err) =>{
-            console.log('Error connecting to MongoDB:' + err);
-            process.exit()
-        })
-    } 
-    catch (error) {
-        console.log('Something went wrong!');
-        console.log(error);
-    }
+    connection.on("connected", () => {
+      console.log("MongoDB connected successfully");
+    });
+    connection.on("error", (err) => {
+      console.log("Error connecting to MongoDB:" + err);
+      process.exit();
+    });
+  } catch (error) {
+    console.log("Something went wrong!");
+    console.log(error);
+  }
 }
