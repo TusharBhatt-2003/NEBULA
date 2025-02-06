cf716d009223a71d6d0d756ae57c74662f233996
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2025-02-07 01:17:09 +0530

    Update

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -2,18 +2,20 @@ import LogoutBtn from "@/app/components/logoutBtn";
 import React from "react";
 
 interface UserProfileProps {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }
 
-export default function UserProfile({ params }: UserProfileProps) {
+export default async function UserProfile({ params }: UserProfileProps) {
+  const { id } = await params; // Await the params before using them
+
   return (
     <div className="flex flex-col items-center justify-center gap-2 min-h-screen py-2">
       <h1>Profile</h1>
       <hr />
       <p className="text-4xl">
-        This your user ID:
+        This is your user ID:
         <span className=" p-1 font-mono text-[#f3f7de] btnBgColor rounded-md mx-2">
-          {params.id}
+          {id}
         </span>
       </p>
       <LogoutBtn />
