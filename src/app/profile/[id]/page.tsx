6ba33c492b9d0ee86cad6d65920ac2a6af46dc77
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2025-02-11 17:22:22 +0530

    Update

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -13,34 +13,36 @@ export default function UserProfile() {
       {loading ? (
         <ProfileSkeleton />
       ) : user ? (
-        <div className="shadow-xl rounded-2xl border-2 border-black">
-          <img
-            src={user.profileUrl}
-            className="w-64 rounded-lg m-3 border-2 border-black"
-          />
-          <div className="m-3">
-            <p className="uppercase text-[#B01018] text-xl font-black">
-              {user.username}
-            </p>
-            <p>{user.email}</p>
-          </div>
-          <div className="flex items-center gap-3 m-3">
-            {user.isverified ? (
-              <div className=" py-1 px-2 text-[#f3f7de] rounded bg-emerald-600">
-                <p>Verified</p>
-              </div>
-            ) : (
-              <div className=" py-1 px-2 rounded bg-green-100">
-                <p>Not Verified</p>
-              </div>
-            )}
-            <p>
-              {user.isAdmin ? (
-                <div className=" py-1 px-2 rounded text-yellow-600 bg-yellow-100">
-                  <p>Admin</p>
+        <div>
+          <div className="shadow-xl rounded-2xl border-2 border-black">
+            <img
+              src={user.profileUrl}
+              className="w-64 rounded-lg m-3 border-2 border-black"
+            />
+            <div className="m-3">
+              <p className="uppercase text-[#B01018] text-xl font-black">
+                {user.username}
+              </p>
+              <p>{user.email}</p>
+            </div>
+            <div className="flex items-center gap-3 m-3">
+              {user.isverified ? (
+                <div className=" py-1 px-2 text-[#f3f7de] rounded bg-emerald-600">
+                  <p>Verified</p>
                 </div>
-              ) : null}
-            </p>
+              ) : (
+                <div className=" py-1 px-2 rounded bg-green-100">
+                  <p>Not Verified</p>
+                </div>
+              )}
+              <p>
+                {user.isAdmin ? (
+                  <div className=" py-1 px-2 rounded text-yellow-600 bg-yellow-100">
+                    <p>Admin</p>
+                  </div>
+                ) : null}
+              </p>
+            </div>
           </div>
           <div className="my-4 w-full flex justify-center items-center">
             <LogoutBtn />
