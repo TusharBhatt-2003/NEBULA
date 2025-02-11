1a00187d08d5b3869e766a6967646c2be56abc40
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2025-02-11 17:39:18 +0530

    Update

--- src/app/components/loading.tsx
+++ src/app/components/loading.tsx
@@ -2,104 +2,59 @@ import React from "react";
 
 export default function Loading() {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 200 200"
-      width="45"
-      height="45"
-    >
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
       <circle
-        fill="#FE3B01"
-        stroke="#FE3B01"
+        fill="#B01018"
+        stroke="#B01018"
         stroke-width="15"
         r="15"
-        cx="35"
+        cx="40"
         cy="100"
       >
         <animate
-          attributeName="cx"
+          attributeName="opacity"
           calcMode="spline"
           dur="2"
-          values="35;165;165;35;35"
-          keySplines="0 .1 .5 1;0 .1 .5 1;0 .1 .5 1;0 .1 .5 1"
+          values="1;0;1;"
+          keySplines=".5 0 .5 1;.5 0 .5 1"
+          repeatCount="indefinite"
+          begin="-.4"
+        ></animate>
+      </circle>
+      <circle
+        fill="#B01018"
+        stroke="#B01018"
+        stroke-width="15"
+        r="15"
+        cx="100"
+        cy="100"
+      >
+        <animate
+          attributeName="opacity"
+          calcMode="spline"
+          dur="2"
+          values="1;0;1;"
+          keySplines=".5 0 .5 1;.5 0 .5 1"
+          repeatCount="indefinite"
+          begin="-.2"
+        ></animate>
+      </circle>
+      <circle
+        fill="#B01018"
+        stroke="#B01018"
+        stroke-width="15"
+        r="15"
+        cx="160"
+        cy="100"
+      >
+        <animate
+          attributeName="opacity"
+          calcMode="spline"
+          dur="2"
+          values="1;0;1;"
+          keySplines=".5 0 .5 1;.5 0 .5 1"
           repeatCount="indefinite"
           begin="0"
-        ></animate>
-      </circle>
-      <circle
-        fill="#FE3B01"
-        stroke="#FE3B01"
-        stroke-width="15"
-        opacity=".8"
-        r="15"
-        cx="35"
-        cy="100"
-      >
-        <animate
-          attributeName="cx"
-          calcMode="spline"
-          dur="2"
-          values="35;165;165;35;35"
-          keySplines="0 .1 .5 1;0 .1 .5 1;0 .1 .5 1;0 .1 .5 1"
-          repeatCount="indefinite"
-          begin="0.05"
-        ></animate>
-      </circle>
-      <circle
-        fill="#FE3B01"
-        stroke="#FE3B01"
-        stroke-width="15"
-        opacity=".6"
-        r="15"
-        cx="35"
-        cy="100"
-      >
-        <animate
-          attributeName="cx"
-          calcMode="spline"
-          dur="2"
-          values="35;165;165;35;35"
-          keySplines="0 .1 .5 1;0 .1 .5 1;0 .1 .5 1;0 .1 .5 1"
-          repeatCount="indefinite"
-          begin=".1"
-        ></animate>
-      </circle>
-      <circle
-        fill="#FE3B01"
-        stroke="#FE3B01"
-        stroke-width="15"
-        opacity=".4"
-        r="15"
-        cx="35"
-        cy="100"
-      >
-        <animate
-          attributeName="cx"
-          calcMode="spline"
-          dur="2"
-          values="35;165;165;35;35"
-          keySplines="0 .1 .5 1;0 .1 .5 1;0 .1 .5 1;0 .1 .5 1"
-          repeatCount="indefinite"
-          begin=".15"
-        ></animate>
-      </circle>
-      <circle
-        fill="#FE3B01"
-        stroke="#FE3B01"
-        stroke-width="15"
-        opacity=".2"
-        r="15"
-        cx="35"
-        cy="100"
-      >
-        <animate
-          attributeName="cx"
-          calcMode="spline"
-          dur="2"
-          values="35;165;165;35;35"
-          keySplines="0 .1 .5 1;0 .1 .5 1;0 .1 .5 1;0 .1 .5 1"
-          repeatCount="indefinite"
-          begin=".2"
         ></animate>
       </circle>
     </svg>
