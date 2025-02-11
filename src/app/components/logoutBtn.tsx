2b9012c134025302ef2fb5d03f8400ee48741c42
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2025-02-11 23:34:33 +0530

    Update

--- src/app/components/logoutBtn.tsx
+++ src/app/components/logoutBtn.tsx
@@ -1,17 +1,18 @@
 "use client";
 import axios from "axios";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React from "react";
 import toast from "react-hot-toast";
 
 export default function LogoutBtn() {
   const router = useRouter();
+
   const logout = async () => {
     try {
       await axios.get("/api/users/logout");
       toast.success("Logged out successfully!");
-      router.push("/login");
+      router.push("/login"); // Redirect to the login page
+      window.location.reload(); // Reload the page to reset the state
     } catch (error: any) {
       console.log(error.message);
       toast.error(error.message);
