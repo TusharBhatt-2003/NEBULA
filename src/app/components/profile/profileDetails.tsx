60a7f35ad616648d953e1bdf5dddf4eb13012be4
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2025-02-28 03:19:22 +0530

    Update

--- src/app/components/profile/profileDetails.tsx
+++ src/app/components/profile/profileDetails.tsx
@@ -4,12 +4,20 @@ interface ProfileDetailsProps {
   birthDate: string;
   city: string;
 }
+
 export const ProfileDetails = ({
   fullName,
   bio,
   birthDate,
   city,
 }: ProfileDetailsProps) => {
+  // Format the birthday date to dd/mm/yy format
+  const formattedDate = new Date(birthDate).toLocaleDateString("en-GB", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "2-digit",
+  });
+
   return (
     <div className="relative py-2 px-3 flex flex-col gap-2 z-10 backdrop-blur rounded-xl border border-[#F2F0E4]/30 overflow-hidden light-text">
       <div className="grain"></div>
@@ -19,7 +27,7 @@ export const ProfileDetails = ({
       </div>
       <p>{bio}</p>
       <div className="flex justify-between items-center">
-        <p className="font-['Big']">{birthDate}</p>
+        <p className="font-['Big']">{formattedDate}</p>
         <p className="bg-black font-['spring'] px-1">{city}</p>
       </div>
     </div>
