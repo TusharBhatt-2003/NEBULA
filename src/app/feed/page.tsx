41ee105ea0c0255c97a104d1e522aa4c0ddda6d0
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2025-03-16 02:54:56 +0530

    Update

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -4,7 +4,7 @@ import Feed from "../components/feed";
 export default function page() {
   return (
     <div className="relative overflow-hidden">
-      <h1 className="overflow-hidden m-5 flex flex-col justify-center relative border-[#F2F0E4]/30 z-10 backdrop-blur-[2px] p-3 light-text font-semibold border rounded-3xl text-center font-['spring'] text-light text-lg">
+      <h1 className="overflow-hidden m-5 mb-0 flex flex-col justify-center relative border-[#F2F0E4]/30 z-10 backdrop-blur-[2px] p-3 light-text font-semibold border rounded-3xl text-center font-['spring'] text-light text-lg">
         <div className="grain"></div>
         The posts from the tags you follow will appear here.
       </h1>
