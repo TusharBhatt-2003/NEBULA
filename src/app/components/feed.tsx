fe8fdd85a0f36e0848ef30fd66a3e44d0647a260
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2025-03-17 00:05:08 +0530

    Update

--- src/app/components/feed.tsx
+++ src/app/components/feed.tsx
@@ -81,7 +81,7 @@ export default function Feed() {
         </div>
 
         <div className="lg:w-[55%] w-full lg:ml-[20vw] text-lg p-5 mb-24 flex flex-col justify-center items-center gap-5 overflow-hidden">
-          {loading && posts
+          {loading && !posts
             ? Array.from({ length: 6 }).map((_, index) => (
                 <Skeleton key={index} />
               ))
