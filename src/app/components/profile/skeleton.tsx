68d89a0615809af60a41876f7401b59548ae3d8a
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2025-03-17 02:19:40 +0530

    Update

--- src/app/components/profile/skeleton.tsx
+++ src/app/components/profile/skeleton.tsx
@@ -3,16 +3,16 @@ import { Button } from "../ui/button";
 export default function Skeleton() {
   return (
     <>
-      <div className="flex gap-2">
-        <div className="relative w-1/3 animate-pulse text-transparent">
-          <div className="backdrop-blur border border-[#F2F0E4]/30 overflow-hidden light-text aspect-square h-full rounded-xl object-cover" />{" "}
+      <div className="flex animate-pulse gap-2">
+        <div className="relative w-1/3  text-transparent">
+          <div className="backdrop-blur light-bg opacity-10 overflow-hidden light-text aspect-square h-full rounded-xl object-cover" />{" "}
           <div className="grain"></div>
         </div>
 
-        <div className="relative animate-pulse w-2/3 py-2 px-3 flex flex-col gap-2 z-10 backdrop-blur rounded-xl border border-[#F2F0E4]/30 overflow-hidden light-text">
+        <div className="relative  w-2/3 py-2 px-3 flex flex-col gap-2 z-10 backdrop-blur rounded-xl border border-[#F2F0E4]/30 overflow-hidden light-text">
           <div className="grain"></div>
           <div className="flex items-center justify-between">
-            <h1 className="text-2xl text-transparent light-bg opacity-25 rounded-3xl animate-pulse">
+            <h1 className="text-2xl text-transparent light-bg opacity-10 rounded-3xl">
               Full Name
             </h1>
             <p className="p-1 text-transparent rounded-xl border border-[#F2F0E4]/30">
@@ -21,20 +21,20 @@ export default function Skeleton() {
           </div>
         </div>
       </div>
-      <div className="backdrop-blur border border-[#F2F0E4]/30 overflow-hidden light-text py-5 px-3 rounded-xl">
+      <div className="backdrop-blur animate-pulse border border-[#F2F0E4]/30 overflow-hidden light-text py-5 px-3 rounded-xl">
         <div className="grain"></div>
-        <p className="light-bg opacity-10 rounded-3xl animate-pulse text-transparent">
+        <p className="light-bg opacity-10 rounded-3xl text-transparent">
           This is the bio of the user, They can write any thing here.
         </p>
       </div>
-      <div className="flex  justify-between">
-        <div className="flex animate-pulse justify-between items-center">
-          <Button variant={"outline"} className="text-transparent">
+      <div className="flex animate-pulse justify-between">
+        <div className="flex justify-between items-center">
+          <Button variant={"ghost"} className="text-transparent">
             Logout
           </Button>
         </div>
-        <div className="flex animate-pulse justify-between items-center">
-          <Button variant="outline" className="text-black">
+        <div className="flex justify-between items-center">
+          <Button variant="ghost" className="text-transparent">
             Update Profile
           </Button>
         </div>
