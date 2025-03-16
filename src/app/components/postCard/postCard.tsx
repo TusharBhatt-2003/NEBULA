88ed5cb7152ce912df2be05e0e2b9d4b57801f61
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2025-03-16 23:14:01 +0530

    Update

--- src/app/components/postCard/postCard.tsx
+++ src/app/components/postCard/postCard.tsx
@@ -171,7 +171,7 @@ export default function PostCard({
 
   return (
     <>
-      {postId ? (
+      {loading && postId ? (
         <motion.div
           initial={{ opacity: 1, y: 20, scale: 0.5 }}
           animate={{ opacity: 1, y: 0, scale: 1 }}
