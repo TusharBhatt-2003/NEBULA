633fa695f8f47b8abc4c92098e1187e5298ca849
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2025-03-21 17:13:46 +0530

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,6 +63,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
         <Navigation />
       </body>
     </html>
